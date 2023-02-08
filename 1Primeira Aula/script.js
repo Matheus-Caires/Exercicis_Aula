aa65a1391d1fff4dc4c9d2337cb46769f5ae7c22
Matheus-Caires <124194580+Matheus-Caires@users.noreply.github.com>
2023-02-08 09:48:19 -0300

    Add files via upload
    exercicios da primeira aula

---
+++ 1Primeira Aula/script.js
@@ -0,0 +1,46 @@
+// Aula 1
+// var numero1 = 10;
+// var numero2 = 20;
+// var total = numero1 + numero2;
+// console.log(total);
+// var sub = numero1 - numero2;
+// console.log(sub);
+// var div = numero1 / numero2;
+// console.log(div);
+// var mult = numero1 * numero2;
+
+// Aula 2
+// console.log(mult);
+// var palavra1 = 'Olá. Bom dia';
+// var palavra2 = ' turma.';
+// var frase = palavra1 + palavra2;
+// alert (frase);
+// document.write(typeof frase);
+
+// Aula 3
+// var nomeUsuario = prompt(' Digite o seu nome: ');
+// document.write(nomeUsuario);
+// var mediaIdade = '25';
+
+// Perguntar dois números para o meu usuário:
+//var numeroUm = parseInt(prompt('Digite o Primeiro número: '));
+//var numeroDois = parseInt(prompt('Digite o Segundo número: '));
+
+// Realizar a soma deles:
+//var soma = numeroUm + numeroDois;
+
+/*  Exibir no corpo da página:
+ document.write(numeroUm + numeroDois);
+ ou
+document.write(soma); */
+
+// Aula 4
+
+ /* var numeroUm = parseFloat(prompt('Digite o primeiro número: '));
+ var numeroDois = parseFloat(prompt('Digite o segundo número: '));
+ var soma = numeroUm + numeroDois; 
+
+alert(soma);*/
+
+// Aula 5
+
